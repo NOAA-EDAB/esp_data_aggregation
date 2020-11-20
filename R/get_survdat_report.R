64d03598af7acd60df317ec93fa5484eb86cbb28
Abigail <abigail.tyrell@noaa.gov>
2020-11-20 15:42:02 -0500

    updated functions

--- R/get_survdat_report.R
+++ R/get_survdat_report.R
@@ -8,23 +8,9 @@ list_species <- split(unique(data$SVSPP), f = list(unique(data$SVSPP)))
 
 # render some reports
 
-test_species <- list_species[1:8] # fails on 008 - not enough data for smoothing?
+test_species <- list_species[9] # fails on 008 - not enough data for smoothing?
 purrr::map(test_species, ~rmarkdown::render(here::here("R", "survdat_report_template.Rmd"), 
                                             params = list(species_ID = .x,
                                                           data_source = data), 
                                             output_dir = here::here("abby_reports"),
                                             output_file = paste(.x, ".html", sep = "")))
-
-# testing geom_gls()
-
-m <- 0.1
-a <- 1:30
-b <-  m*a + rnorm(30, sd = 0.35)
-
-data2 <- data.frame(x = x,
-                   y = y)
-
-#Plot series with trend 
-ggplot2::ggplot(data = data2,aes(x = a, y = b)) +
-  geom_line() +
-  ecodata::geom_gls()
