0f63576a3c7437b596933bd151680a73075a3783
Abigail <abigail.tyrell@noaa.gov>
2020-11-23 11:21:01 -0500

    change code to save reports to docs

--- R/get_survdat_report.R
+++ R/get_survdat_report.R
@@ -8,9 +8,9 @@ list_species <- split(unique(data$SVSPP), f = list(unique(data$SVSPP)))
 
 # render some reports
 
-test_species <- list_species[9] # fails on 008 - not enough data for smoothing?
+test_species <- list_species[9:20] # fails on 008 - not enough data for smoothing?
 purrr::map(test_species, ~rmarkdown::render(here::here("R", "survdat_report_template.Rmd"), 
                                             params = list(species_ID = .x,
                                                           data_source = data), 
-                                            output_dir = here::here("abby_reports"),
+                                            output_dir = here::here("docs"),
                                             output_file = paste(.x, ".html", sep = "")))
