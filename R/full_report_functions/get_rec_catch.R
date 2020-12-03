d56afc5c7152ed1eaf73771aee526fa780956976
Abigail <abigail.tyrell@noaa.gov>
2020-12-03 17:44:37 -0500

    add zeros, fix colors (in progress)

--- R/full_report_functions/get_rec_catch.R
+++ R/full_report_functions/get_rec_catch.R
@@ -2,26 +2,41 @@
 library(ggplot2)
 
 get_rec_catch <- function(data){
-  
-  data <- dplyr::filter(data)
-  
+
   summary <- data %>%
-    dplyr::filter(sub_reg_f == "NORTH ATLANTIC") %>%
     dplyr::group_by(mode_fx_f, year) %>%
     dplyr::summarise(total_catch = sum(tot_cat))
   
-  fig <- ggplot(summary,
+  # add in zeros
+  combo <- expand.grid(year = min(summary$year):max(summary$year),
+                       mode_fx_f = unique(summary$mode_fx_f))
+  
+  summary2 <- dplyr::full_join(summary, combo, 
+                               by = c("year" = "year",
+                                      "mode_fx_f" = "mode_fx_f")) %>%
+    dplyr::mutate(total_catch2 = ifelse(is.na(total_catch), 0, total_catch))
+  
+  # get order of most important - least important category
+  cat <- summary2 %>% dplyr::group_by(mode_fx_f) %>%
+    dplyr::summarise(imp = max(total_catch2)) %>%
+    dplyr::arrange(dplyr::desc(imp))
+  
+  summary2$mode_fx_f <- factor(summary2$mode_fx_f, 
+                               cat$mode_fx_f)
+  
+  # plot
+  fig <- ggplot(summary2,
                 aes(x = year,
-                    y = total_catch,
+                    y = total_catch2,
                     fill = mode_fx_f))+
     geom_area()+
     theme_bw()+
     scale_y_continuous(name = "Total catch (lb)",
                        labels = scales::comma)+
     xlab("Year")+
-    labs(fill = "Category")+
+    ggsci::scale_fill_jco()+
+    guides(fill=guide_legend(nrow=2, byrow = TRUE, title = "Category"))+
     theme(legend.position = "bottom")
   
   return(fig)
-  
 }
