8ce80b32967c3ec908e203e5f1347356f2f87548
Abigail <abigail.tyrell@noaa.gov>
2020-12-11 12:59:49 -0500

    fix MRIP data

--- R/create_data/create_report_data.R
+++ R/create_data/create_report_data.R
@@ -152,15 +152,20 @@ write.csv(data, file = here::here("data", "geo_range_data.csv"))
 #####
 files <- dir(here::here("data/MRIP"))
 read_files <- files[stringr::str_detect(files, "catch_year") %>% which()]
+col_to_keep <-  read.csv(here::here("data/MRIP", read_files[1])) %>%
+  colnames()
 
 big_data <- c()
 for(i in 1:length(read_files)){
-  this_data <- read.csv(here::here("data/MRIP", read_files[i]))
+  this_data <- read.csv(here::here("data/MRIP", read_files[i])) %>%
+    tibble::as_tibble() %>%
+    dplyr::select(col_to_keep)
   big_data <- rbind(big_data, this_data)
 }
 head(big_data)
+tail(big_data)
 
-big_data$tot_cat <- stringr::str_replace(big_data$tot_cat, ",", "") %>%
+big_data$tot_cat <- stringr::str_replace_all(big_data$tot_cat, ",", "") #%>%
   as.numeric()
 big_data$Species <- stringr::str_to_sentence(big_data$common)
 
