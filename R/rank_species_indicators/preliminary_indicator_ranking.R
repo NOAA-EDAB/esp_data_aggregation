eb641fd1ae9137bbf64253224b1318db0d01c956
Abigail <abigail.tyrell@noaa.gov>
2020-12-14 16:14:20 -0500

    more preliminary risk ranking

--- R/rank_species_indicators/preliminary_indicator_ranking.R
+++ R/rank_species_indicators/preliminary_indicator_ranking.R
@@ -116,20 +116,56 @@ asmt$Species <- asmt$Species %>%
 asmt$Units <- asmt$Units %>%
   stringr::str_replace("Thousand Recruits", "Number x 1,000")
 
-catch_rank  <- dplyr::filter(Metric == "Catch") %>%
+# standardize units
+for(i in 1:nrow(asmt)){
+  if(asmt$Units[i] == "Thousand Metric Tons"){
+    asmt$Value [i] <- 1000 * asmt$Value[i]
+    asmt$Units[i] <- "Metric Tons"
+  }
+}
+
+catch_rank  <- asmt %>% dplyr::filter(Metric == "Catch") %>%
   dplyr::mutate(ne_stock = (Species %in% key2$Species)) %>%
   dplyr::filter(ne_stock == "TRUE") %>%
-  
-  dplyr::group_by(Species) %>%
-  dplyr::mutate(max_catch = mean(tot_cat)) 
-dplyr::mutate(Year_measured = paste("average of", 
-                                    max_year - 5, "-", max_year, 
-                                    "data"),
-              Indicator = "rec_catch",
-              rank = abs(rank(avg_catch_5yr) - max(rank(avg_catch_5yr) + 1)), 
-              norm_rank = rank/max(rank)) %>%
-  dplyr::rename(Value = avg_catch_5yr)
+  dplyr::group_by(Species, Region, Units) %>%
+  dplyr::mutate(max_catch = max(Value)) %>%
+  dplyr::filter(Value == max_catch) %>%
+  dplyr::mutate(Indicator = "total_catch") %>%
+  dplyr::rename("Year_measured" = "Year")
 catch_rank
+
+catch_rank <- catch_rank %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(rank = rank(Value), 
+                norm_rank = rank/max(rank)) %>%
+  dplyr::select(Species, Region, Value, Year_measured, Indicator, rank, norm_rank)
+
+# avg abundance of past 10 years as % of avg abundance of all times up to 10 years ago
+max(asmt$Year)
+abun_rank  <- asmt %>% dplyr::filter(Metric == "Abundance", Units == "Metric Tons") %>%
+  dplyr::mutate(ne_stock = (Species %in% key2$Species),
+                recent = Year > max(Year) - 10) %>%
+  dplyr::filter(ne_stock == "TRUE") %>%
+  dplyr::group_by(Species, Region, Description, recent) %>%
+  dplyr::mutate(mean_abun = mean(Value)) %>%
+  dplyr::mutate(Indicator = "abundance") %>%
+  dplyr::select(Species, Region, recent, mean_abun) %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(names_from = recent,
+                     values_from = mean_abun,
+                     names_prefix = "recent_") %>%
+  dplyr::mutate(abun_score = recent_TRUE/recent_FALSE) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(rank =  abs(rank(abun_score) - max(rank(abun_score) + 1)), 
+                norm_rank = rank/max(rank)) %>%
+  dplyr::select(Species, Region, abun_score, rank, norm_rank) %>%
+  dplyr::rename("Value" = "abun_score") %>%
+  dplyr::mutate(Year_measured = "mean of last 10 years vs historic mean",
+                Indicator = "abundance") %>%
+  dplyr::select(Species, Region, Value, Year_measured, Indicator, rank, norm_rank)
+abun_rank
+
+
 
 # additional indicators to rank ----
 
@@ -137,7 +173,7 @@ catch_rank
 # range of temperatures found at
 # abs value of salinity change (surface-bottom x spring-fall)
 # range of salinities found at
-# avg abundance of past 10 years as % of avg abundance of all times up to 10 years ago
+
 # avg biomass of past 10 years as % of avg biomass of all times up to 10 years ago
 # avg recruitment of past 10 years as % of avg recruitment of all times up to 10 years ago
 # avg length of past 10 years as % of avg length of all times up to 10 years ago
@@ -148,7 +184,7 @@ catch_rank
 # plot ----
 
 # plot total risk
-data <- rbind(f_rank, b_rank)
+data <- rbind(f_rank, b_rank, catch_rank, abun_rank)
 
 head(data)
 
@@ -167,6 +203,7 @@ data2 <- data2 %>%
   dplyr::group_by(stock) %>%
   dplyr::mutate(total_risk = sum(norm_rank, na.rm = TRUE))
 
+# first plot
 ggplot(data2,
        aes(x = reorder(stock, -total_risk),
            y = norm_rank,
@@ -188,8 +225,69 @@ ggplot(data2,
         panel.grid.minor.x = element_blank()
         )
 
+data2 <- data2 %>% dplyr::filter(is.na(Value) == FALSE)
+head(data2)
+
+# missing data = risk of 0.5
+data3 <- data2 %>%
+  tidyr::pivot_wider(names_from = "Indicator",
+                     values_from = "norm_rank") %>%
+  dplyr::group_by(Species, Region, stock) %>%
+  dplyr::summarise(ffmsy_new = sum(ffmsy, na.rm = TRUE),
+                   bbmsy_new = sum(bbmsy, na.rm = TRUE),
+                   total_catch_new = sum(total_catch, na.rm = TRUE),
+                   rec_catch_new = sum(rec_catch, na.rm = TRUE),
+                   abun_new = sum(abundance, na.rm = TRUE)) %>%
+  dplyr::mutate(ffmsy_new = ifelse(ffmsy_new == 0, 0.5, ffmsy_new),
+                bbmsy_new = ifelse(bbmsy_new == 0, 0.5, bbmsy_new),
+                total_catch_new = 
+                  ifelse(total_catch_new == 0, 0.5, total_catch_new),
+                total_catch_new = 
+                  ifelse(total_catch_new == 0, 0.5, total_catch_new),
+                abun_new = ifelse(abun_new == 0, 0.5, abun_new)) %>%
+  dplyr::mutate(total_risk = 
+                  ffmsy_new + bbmsy_new + total_catch_new + rec_catch_new + abun_new) %>%
+  tidyr::pivot_longer(cols = c(ffmsy_new, bbmsy_new, total_catch_new, rec_catch_new, abun_new))
+data3
+
+data3$label <- paste(data3$Species, 
+                     data3$total_risk %>% round(digits = 2), 
+                     sep = ", ")
+
+fig <- ggplot(data3,
+       aes(x = reorder(stock, -total_risk),
+           y = name,
+           fill = value))+
+  geom_raster(stat = "identity")+
+  geom_text(aes(x = reorder(stock, -total_risk),
+                y = 0.5,
+                label = label),
+            angle = 90,
+            hjust = 0,
+            cex = 3,
+            color = "black")+
+  theme_bw()+
+  scale_fill_gradient2(high = "darkred", mid = "beige", low = "forestgreen", midpoint = 0.5)+
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.position = "bottom"
+  )+
+  facet_wrap(facets = vars(Region),
+             scales = "free_x")
+fig
+
+pdf(here::here("docs/risk_ranking", "preliminary_risk_ranking.pdf"),
+    height = 6.5, width = 9)
+fig
+dev.off()
+
 #
-write.csv(data2, here::here("docs/risk_ranking", "preliminary_risk_ranking.csv"))
+write.csv(data3, here::here("docs/risk_ranking", "preliminary_risk_ranking.csv"))
+
+read.csv(here::here("docs/risk_ranking", "preliminary_risk_ranking.csv")) %>% head
 
 
 
