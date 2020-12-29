9b8be883947022f860e811f718b17c1138a37206
rtabandera <ricky.tabandera@noaa.gov>
2020-12-28 15:14:03 -1000

    Render all of the commercial reports

---
+++ Commercial/render_commercial_landings report.R
@@ -0,0 +1,27 @@
+
+
+all_species<-c(        "smooth dogfish",          "spiny dogfish",           "barndoor skate",          "winter skate",           
+"clearnose skate",           "little skate",            "smooth skate",            "thorny skate",           
+"atlantic herring",       "alewife",                 "blueback herring",        "offshore hake" ,          "silver hake",            
+"atlantic cod",            "haddock",                 "pollock",                 "white hake",              "red hake",               
+"cusk",                    "atlantic halibut",        "american plaice",         "summer flounder",         "yellowtail flounder",    
+"winter flounder",         "witch flounder",          "atlantic mackerel",       "butterfish",              "bluefish",               
+"black sea bass",          "scup",                   "acadian redfish",        "atlantic wolffish",       "ocean pout",             
+ "monkfish",                "american lobster" ,       "northern shrimp",         "northern shortfin squid", "longfin inshore squid",  
+ "atlantic menhaden",       "windowpane flounder"  )
+
+#"rosette skate" is removed as it does not contain any records 
+
+library("here")
+
+all_species<-stringr::str_sort(all_species)
+all_species<-c("white hake")
+
+list_species <- split(all_species, f = list(all_species))
+
+purrr::map(list_species, ~rmarkdown::render(here::here("Commercial", "Commercial_landings_1950_2019.Rmd"), 
+                                            params = list(stock = .x), 
+                                            output_dir = here::here("docs","Commercial landings"),
+                                            output_file = paste(.x, "_landings", 
+                                                                ".html", sep = "")))
+
--- Recreational/render_recreational_landings_report.R
+++ Recreational/render_recreational_landings_report.R
@@ -6,7 +6,7 @@ all_species<-c("smooth dogfish","spiny dogfish", "barndoor skate", "winter skate
 "alewife", "blueback herring",    "silver hake",  "atlantic cod",       "haddock", "pollock",   "white hake",  "red hake", "cusk",  "atlantic halibut", "american plaice", "summer flounder", "yellowtail flounder", "winter flounder",     "atlantic mackerel",  
 "butterfish", "bluefish", "black sea bass", "scup",  "acadian redfish", "atlantic wolffish", "ocean pout",   "atlantic menhaden",  "windowpane flounder")
 
-
+list_species <- split(all_species, f = list(all_species))
 
 purrr::map(list_species, ~rmarkdown::render(here::here("Recreational", "recreational_landings_1950_2019.Rmd"), 
                                                params = list(stock = .x), 
