68630e7f1bc22ad8db07453199954f2fec7806a4
Abigail <abigail.tyrell@noaa.gov>
2021-01-06 12:42:37 -0500

    create bookdown reports

---
+++ R/install_git_packages.R
@@ -0,0 +1,19 @@
+# all packages installed from git
+
+remotes::install_github("NOAA-EDAB/survdat",
+                        build_vignettes = TRUE)
+
+remotes::install_github("andybeet/dbutils")
+
+remotes::install_github("NOAA-EDAB/assessmentdata",
+                        build_vignettes = TRUE)
+
+remotes::install_github("NOAA-EDAB/ecodata",
+                        build_vignettes = TRUE)
+
+remotes::install_github("ropensci/rnaturalearthhires",
+                        build_vignettes = TRUE)
+
+remotes::install_github("nmfs-general-modeling-tools/nmfspalette",
+                        build_vignettes = TRUE)
+
