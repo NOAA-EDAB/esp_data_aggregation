bd36f22deacd5af639b997ba062b974c1c635ebf
rtabandera <ricky.tabandera@noaa.gov>
2021-01-13 13:53:01 -1000

    pivoted and visualized

--- Life history/Lifehistory.R
+++ Life history/Lifehistory.R
@@ -1,8 +1,13 @@
 #organizing life history parameters 
+install.packages("FSA")
+install.packages("ggrepel")
 
 devtools::install_github("james-thorson/FishLife")
 library( FishLife )
 library(tidyverse)
+library(ggrepel)
+library(FSA)
+library(rfishbase)
 
 stock_list_all_strata<- readr::read_csv("https://raw.githubusercontent.com/NOAA-EDAB/ECSA/master/data/stock_list.csv")
 stock_list<-stock_list_all_strata %>% dplyr::distinct( common_name, .keep_all=TRUE)
@@ -42,10 +47,7 @@ stock_list<-stock_list_all_strata %>% dplyr::distinct( common_name, .keep_all=TR
 
 
 
-install.packages("FSA")
-library(FSA)
 
-library(rfishbase)
 t2<-fecundity("Gadus morhua")
 t3<-maturity("Gadus morhua")
 t4<-fooditems("Gadus morhua")
@@ -72,13 +74,13 @@ write.csv(predictions_join, file = "predictions_join.csv")
 
 
 
-
+#function to calculate the fitted growth curve for von bertalanffy growth curve
 vonb<-function(t,Linf,K,tzero){Latthisage<-Linf*(1-exp(-K*(t-tzero)))
   return(Latthisage)
 }
 
 
-#Pauly 1979) to estimate a default value for t0 as log (-t0) = -0.3922 - 0.2752 log Linf - 1.038 log K
+#Pauly 1979 based method to estimate a default value for t0 as log (-t0) = -0.3922 - 0.2752 log Linf - 1.038 log K
 #consistant way of generating t0  
 tzero<-function(Linf,K){logtzero<-(-0.3922 - (0.2752*log(Linf) - 1.038*log(K)))
   t0<-exp(logtzero)
@@ -98,4 +100,20 @@ spec.age.growth.list<-map(flist, ~vonb(seq(1:50),.x[[2]],.x[[3]],.x[[4]]))
 
 
 spec.age.growth<-do.call(cbind,spec.age.growth.list)
+
+spec.age.growth.df<-data.frame(spec.age.growth)
+fish.names<-colnames(spec.age.growth.df)
+
+
+
+spec.age.growth.df$year<-seq(1:50)
+
+age.growth.curves<-spec.age.growth.df %>% pivot_longer(cols = fish.names,names_to = "species", values_to = "length" )
+
+
+
+age.growth.curves %>% ggplot(aes(x=year, y=length,label = species))+geom_line(aes(color=species))+geom_label_repel(box.padding = 0.5, max.overlaps = Inf)
+
+
+
 
