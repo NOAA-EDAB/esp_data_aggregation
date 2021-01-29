fecfe0c06c8ed950b8bf715799e8ea60b711db9e
Abigail <abigail.tyrell@noaa.gov>
2021-01-29 17:55:31 -0500

    update buildignore

--- remove_files.R
+++ remove_files.R
@@ -5,7 +5,8 @@ files <- list.files()
 keep <- which(files == "docs")
 remove <- files[-keep]
 
-file.remove(remove)
-unlink(remove, recursive = TRUE)
+# do NOT run this code locally!!! for github actions only!
+#file.remove(remove)
+#unlink(remove, recursive = TRUE)
 
-warnings()
+usethis::use_build_ignore(remove, escape = TRUE)
