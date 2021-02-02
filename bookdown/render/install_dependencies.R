b577ba266bcf4f8d471f9d5cf58931f0971b6a0f
Abigail <abigail.tyrell@noaa.gov>
2021-02-02 15:22:33 -0500

    update dependencies

--- bookdown/render/install_dependencies.R
+++ bookdown/render/install_dependencies.R
@@ -1,5 +1,8 @@
 # these packages have to be installed for other packages to get installed
-install.packages(c("remotes", "ggplot2", "nlstools", "rgeos", "viridis", "ggthemes"))
+#install.packages(c("remotes", "ggplot2", "nlstools", "rgeos", "viridis", "ggthemes", "here"))
+
+install.packages("devtools")
+devtools::install_deps()
 
 # install github packages
 remotes::install_github("NOAA-EDAB/assessmentdata")
@@ -13,6 +16,11 @@ remotes::install_github("ropensci/rnaturalearth")
 remotes::install_github("nmfs-general-modeling-tools/nmfspalette")
 
 # load all packages
-renv::restore(lockfile = here::here("bookdown", "renv.lock"))
+#renv::restore(lockfile = here::here("bookdown", "renv.lock"))
 
-install.packages("parallel")
+#install.packages("parallel")
+
+#my_packages <- library()$results[,1]
+#my_packages <- paste(my_packages, cat = ", ")
+#usethis::use_description(fields = list(Suggests = my_packages),
+#                         check_name = FALSE)
