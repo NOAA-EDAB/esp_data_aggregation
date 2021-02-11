1a3bc0265c71b37b3b2d853975ffbe062da072d5
Abigail <abigail.tyrell@noaa.gov>
2021-02-11 18:02:40 -0500

    testing bluefish regressions

--- R/Bluefish/bluefish.R
+++ R/Bluefish/bluefish.R
@@ -18,30 +18,65 @@ ggplot(ecodata::cold_pool,
 data <- dplyr::left_join(bluefish, ecodata::cold_pool,
                          by = "Time")
 
-ggplot(data %>%
-         dplyr::filter(is.na(Var) == FALSE),
-       aes(x = Val,
-           y = Value))+
-  geom_line()+
-  stat_smooth(method = "lm")+
-  facet_grid(rows = vars(Metric),
-             cols = vars(Var),
-             scales = "free")+
-  theme_bw()
+plot_correlation <- function(bluefish, eco, lag){
+  bluefish2 <- bluefish %>%
+    dplyr::mutate(Time = Time + lag)
+  
+  data <- dplyr::left_join(bluefish2, eco,
+                           by = "Time") %>%
+    dplyr::filter(is.na(Var) == FALSE)
 
-data2 <- dplyr::left_join(bluefish %>%
-                           dplyr::mutate(Time = Time + 1), # try a lag
-                         ecodata::cold_pool,
-                         by = "Time")
+  fig <- ggplot(data,
+                aes(x = Val,
+                    y = Value))+
+    geom_line(lty = 2)+
+    geom_point()+
+    stat_smooth(method = "lm")+
+    facet_grid(rows = vars(Metric),
+               cols = vars(Var),
+               scales = "free")+
+    theme_bw()
+  
+  # test correlations
 
-ggplot(data2 %>%
-         dplyr::filter(is.na(Var) == FALSE)
-       ,
-       aes(x = Val,
-           y = Value))+
-  geom_line()+
-  stat_smooth(method = "lm")+
-  facet_grid(rows = vars(Metric),
-             cols = vars(Var),
-             scales = "free")+
-  theme_bw()
+  for(i in unique(data$Metric)){
+    for(j in unique(data$Var)){
+      dat <- data %>%
+        dplyr::filter(Metric == i, Var == j)
+
+      results <- lm(Value ~ Val, 
+                    data = dat) %>%
+        summary
+
+      if(results[[4]][2,4] < 0.05){
+        print(paste(i, j, sep = " vs "))
+        print(results)
+        
+      }
+      
+
+    }
+  }
+  
+  return(fig)
+}
+
+plot_correlation(bluefish = bluefish, 
+                 eco = ecodata::cold_pool, 
+                 lag = 0)
+
+plot_correlation(bluefish = bluefish, 
+                 eco = ecodata::heatwave %>%
+                   dplyr::rename(Val = Value), 
+                 lag = 0)
+
+plot_correlation(bluefish = bluefish, 
+                 eco = ecodata::bottom_temp_glorys %>%
+                   dplyr::rename(Val = Value), 
+                 lag = 0)
+
+plot_correlation(bluefish = bluefish, 
+                 eco = ecodata::forage_anomaly %>%
+                   dplyr::rename(Val = Value), 
+                 lag = 0)
+
