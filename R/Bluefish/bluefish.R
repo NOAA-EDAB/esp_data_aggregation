1cfe9882a6fbb10c4abe1eca69c6e6748ca15b24
Abigail <abigail.tyrell@noaa.gov>
2021-02-12 16:14:54 -0500

    preliminary bluefish indicator analysis

--- R/Bluefish/bluefish.R
+++ R/Bluefish/bluefish.R
@@ -24,18 +24,26 @@ plot_correlation <- function(bluefish, eco, lag){
   
   data <- dplyr::left_join(bluefish2, eco,
                            by = "Time") %>%
-    dplyr::filter(is.na(Var) == FALSE)
+    dplyr::filter(is.na(Var) == FALSE) %>%
+    dplyr::group_by(Metric, Var) %>%
+    dplyr::mutate(pval = coefficients(summary(lm(Value ~ Val)))[2,4],
+                  sig = pval < 0.05)
 
   fig <- ggplot(data,
                 aes(x = Val,
-                    y = Value))+
+                    y = Value,
+                    color = sig))+
     geom_line(lty = 2)+
     geom_point()+
     stat_smooth(method = "lm")+
     facet_grid(rows = vars(Metric),
                cols = vars(Var),
                scales = "free")+
-    theme_bw()
+    scale_color_manual(values = c("black", "#B2292E"),
+                       name = "Statistically significant\n(p < 0.05)")+
+    theme_bw()+
+    theme(axis.title = element_blank(),
+          legend.position = "bottom")
   
   # test correlations
 
@@ -51,10 +59,7 @@ plot_correlation <- function(bluefish, eco, lag){
       if(results[[4]][2,4] < 0.05){
         print(paste(i, j, sep = " vs "))
         print(results)
-        
       }
-      
-
     }
   }
   
---
+++ R/Bluefish/indicator_functions.R
@@ -0,0 +1,101 @@
+plot_correlation <- function(bluefish, eco, lag){
+  # both data sets must have a column called "Time"
+  # the bluefish data should be from assessmentdata::stockAssessmentData
+  # the eco data numeric values should be in a column called "Val"
+  # the eco data category values should be in a column called "Var"
+  
+  bluefish2 <- bluefish %>%
+    dplyr::mutate(Time = Time + lag,
+                  facet = paste(Metric, Description, Units, sep = "\n"))
+  
+  data <- dplyr::left_join(bluefish2, eco,
+                           by = "Time") %>%
+    dplyr::filter(is.na(Var) == FALSE) %>%
+    dplyr::group_by(Metric, Var) %>%
+    dplyr::mutate(pval = coefficients(summary(lm(Value ~ Val)))[2,4],
+                  sig = pval < 0.05)
+  
+  fig <- ggplot(data,
+                aes(x = Val,
+                    y = Value,
+                    color = sig))+
+    geom_line(lty = 2)+
+    geom_point()+
+    stat_smooth(method = "lm")+
+    facet_grid(rows = vars(facet),
+               cols = vars(Var),
+               scales = "free")+
+    scale_color_manual(values = c("black", "#B2292E"),
+                       name = "Statistically significant\n(p < 0.05)")+
+    scale_y_continuous(labels = scales::comma)+
+    theme_bw()+
+    theme(axis.title = element_blank(),
+          legend.position = "bottom")
+  
+  return(fig)
+}
+
+#plot_correlation(bluefish = bluefish, 
+#                 eco = ecodata::forage_anomaly %>%
+#                   dplyr::rename(Val = Value), 
+#                 lag = 0)
+
+correlation_data <- function(bluefish, eco, lag){
+  bluefish2 <- bluefish %>%
+    dplyr::mutate(Time = Time + lag,
+                  facet = paste(Metric, Description, Units, sep = "\n"))
+  
+  data <- dplyr::left_join(bluefish2, eco,
+                           by = "Time") %>%
+    dplyr::filter(is.na(Var) == FALSE) %>%
+    dplyr::group_by(Metric, Var) %>%
+    dplyr::mutate(pval = coefficients(summary(lm(Value ~ Val)))[2,4],
+                  sig = pval < 0.05)
+  
+  # test correlations
+  
+  if(min(data$pval) >= 0.05){
+    print("No statistically significant results")
+  } else {
+    for(i in unique(data$Metric)){
+    for(j in unique(data$Var)){
+      dat <- data %>%
+        dplyr::filter(Metric == i, Var == j)
+      
+      results <- lm(Value ~ Val, 
+                    data = dat) %>%
+        summary
+      
+      if(results[[4]][2,4] < 0.05){
+       # print(paste(i, j, sep = " vs "))
+        knitr::kable(results$coefficients %>%
+                       round(digits = 2),
+                     col.names = c("Estimate", "St Error", 
+                                   "t-value", "p-value"),
+                     caption = paste(i, j, sep = " vs ")) %>%
+          kableExtra::kable_styling(full_width = FALSE) %>%
+          print()
+        
+        knitr::kable(data.frame(Name = c("F-statistic", "df", "R2", "R2-adj"),
+                                Value = c(results$fstatistic[1] %>%
+                                            round(digits = 2), 
+                                          paste(results$fstatistic[2:3], 
+                                                collapse = ", "),
+                                          results$r.squared %>%
+                                            round(digits = 2), 
+                                          results$adj.r.squared %>%
+                                            round(digits = 2)
+                                ))) %>%
+          kableExtra::kable_styling(full_width = FALSE) %>%
+          print()
+        }
+      }
+    }
+  }
+}
+
+render_indicator <- function(test){
+  res <- knitr::knit_child(here::here("R/Bluefish", "bluefish-child-doc.Rmd"), 
+                           quiet = TRUE)
+  cat(res, sep = '\n')
+}
