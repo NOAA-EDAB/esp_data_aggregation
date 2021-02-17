bfb4a6fb024adbec191073a23c672ded833beaa4
Abigail <abigail.tyrell@noaa.gov>
2021-02-16 19:00:57 -0500

    bluefish esp

--- R/Bluefish/render_bookdown.R
+++ R/Bluefish/render_bookdown.R
@@ -1,10 +1,12 @@
 setwd(here::here("R/Bluefish/bookdown"))
 bookdown::render_book(input = ".",
+                      params = list(lag = 0),
+                      output_dir = "lag0_book",
                       clean = FALSE,
                       quiet = FALSE) 
 
 bookdown::render_book(input = ".",
-                      params = list(lag = 1)
+                      params = list(lag = 1),
                       output_dir = "lag1_book",
                       clean = FALSE,
                       quiet = FALSE) 
