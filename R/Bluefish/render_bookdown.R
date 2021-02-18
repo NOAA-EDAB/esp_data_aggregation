605ee16a34de694ac80b4878d13083b44e18e9f1
Abigail <abigail.tyrell@noaa.gov>
2021-02-17 19:23:00 -0500

    exploring bluefish regressions

--- R/Bluefish/render_bookdown.R
+++ R/Bluefish/render_bookdown.R
@@ -1,12 +1,19 @@
 setwd(here::here("R/Bluefish/bookdown"))
 bookdown::render_book(input = ".",
                       params = list(lag = 0),
-                      output_dir = "lag0_book",
-                      clean = FALSE,
+                      output_dir = "Bluefish_lag0_book",
+                      clean = TRUE,
                       quiet = FALSE) 
 
 bookdown::render_book(input = ".",
                       params = list(lag = 1),
-                      output_dir = "lag1_book",
-                      clean = FALSE,
+                      output_dir = "Bluefish_lag1_book",
+                      clean = TRUE,
+                      quiet = FALSE) 
+
+bookdown::render_book(input = ".",
+                      params = list(lag = 1,
+                                    remove_recent = TRUE),
+                      output_dir = "Bluefish_lag1_remove_recent_book",
+                      clean = TRUE,
                       quiet = FALSE) 
