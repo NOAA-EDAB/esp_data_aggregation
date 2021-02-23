42729a23928071c2d92271930f545a291cea7c95
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2021-02-23 11:52:40 -0500

    don't render broken report(s)

--- R/regressions/render_regression_reports_ghaction.R
+++ R/regressions/render_regression_reports_ghaction.R
@@ -61,8 +61,8 @@ render_reg_report <- function(stock_var, epus_var, region_var, remove_var,
               sep = ": "))
 }
 
-for(i in #1
-    1:nrow(info)
+for(i in 1:22
+    #1:nrow(info)
     ){
   # make 0 lag reports
   render_reg_report(stock_var = info[i, 1], 
