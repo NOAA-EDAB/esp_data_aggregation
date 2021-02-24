f65a0e12ad8b86c42f065d5448f405c037d0f0b4
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2021-02-24 09:20:12 -0500

    fix typo

--- R/regressions/render_regression_reports_ghaction.R
+++ R/regressions/render_regression_reports_ghaction.R
@@ -17,8 +17,9 @@ render_reg_report <- function(stock_var, epus_var, region_var, remove_var,
                               lag_var, parent_folder, trouble){
   
   new_dir <- here::here("Regressions", parent_folder, 
-                        paste(stock_var, region_var, epus_var, 
-                              %>% stringr::str_replace_all("/", "-"))) %>%
+                        paste(stock_var, region_var, 
+                              %>% stringr::str_replace_all("/", "-"),
+                             epus_var,)) %>%
     stringr::str_replace_all(" ", "_")
   dir.create(new_dir, 
              recursive = TRUE)
