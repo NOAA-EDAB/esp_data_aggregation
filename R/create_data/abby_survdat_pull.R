6379eb634b24e78b41df26820a416727e41fbd38
Abigail <abigail.tyrell@noaa.gov>
2021-03-01 09:57:43 -0500

    format new survdat data pull

--- R/create_data/abby_survdat_pull.R
+++ R/create_data/abby_survdat_pull.R
@@ -8,7 +8,7 @@ pull <- survdat::get_survdat_data(channel,
                           all.season = TRUE,
                           bio = TRUE)
 
-saveRDS(pull, here::here("data", "survdat_02012021.RDS"))
+saveRDS(pull, here::here("data", "survdat_03012021.RDS"))
 # still too short :(
 
 pull <- readRDS(here::here("data", "survdat_122920.RDS"))
