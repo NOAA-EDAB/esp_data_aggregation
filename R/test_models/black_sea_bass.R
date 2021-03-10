948c9f2b81bdc0eaaaf70ffc5907018d44bf7a25
Abigail <abigail.tyrell@noaa.gov>
2021-03-10 17:34:49 -0500

    testing report rendering

--- R/test_models/black_sea_bass.R
+++ R/test_models/black_sea_bass.R
@@ -1,1 +1,17 @@
 NEesp::render_ind_report("Black sea bass", trouble = TRUE)
+
+NEesp::render_reg_report(stock_var = "Black sea bass", 
+                         epus_var = "MAB",
+                         region_var = "Mid",
+                         trouble = TRUE,
+                         parent_folder = "lag0")
+
+NEesp::render_reg_report(stock_var = "Black sea bass", 
+                         epus_var = "MAB",
+                         region_var = "Mid",
+                         trouble = TRUE,
+                         lag_var = 1,
+                         parent_folder = "lag1")
+
+#
+NEesp::render_ind_report("Black sea bass", trouble = TRUE, input = "bookdown")
