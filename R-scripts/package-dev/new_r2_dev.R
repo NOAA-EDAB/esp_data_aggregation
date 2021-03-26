8ac061fb5b45330ab05a2a9e7ffd35acf3f7e776
Abigail <abigail.tyrell@noaa.gov>
2021-03-26 14:04:53 -0400

    testing change to glm

---
+++ R-scripts/package-dev/new_r2_dev.R
@@ -0,0 +1,14 @@
+dat <- data.frame(x = rpois(10, 5),
+                  y = rpois(10, 5) * 2 + rpois(1, 1))
+
+model <- glm(y ~ x, data = dat, family = poisson())
+results <- summary(model)
+
+results$coefficients[2, 1] # slope
+results$coefficients[2, 4] # pval
+results$adj.r.squared 
+
+pseudoR2 <- function(model, data, ind_colname){
+  new_data <- data %>%
+    dplyr::select(ind_colname)
+}
