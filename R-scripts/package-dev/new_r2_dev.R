5d40fb83241d1e79161134c26f2c788d925cc8f1
Abigail <abigail.tyrell@noaa.gov>
2021-03-29 11:25:04 -0400

    pseudo r2 for poisson glm

--- R-scripts/package-dev/new_r2_dev.R
+++ R-scripts/package-dev/new_r2_dev.R
@@ -1,14 +1,29 @@
 dat <- data.frame(x = rpois(10, 5),
-                  y = rpois(10, 5) * 2 + rpois(1, 1))
+                  y = rpois(10, 5) * 10 + rpois(1, 1))
 
-model <- glm(y ~ x, data = dat, family = gaussian())
-results <- summary(model)
+mod <- glm(y ~ x, data = dat, family = poisson())
+mod <- lm(y ~x, data = dat)
+results <- summary(mod)
 
 results$coefficients[2, 1] # slope
 results$coefficients[2, 4] # pval
 results$adj.r.squared 
 
-pseudoR2 <- function(model, data, ind_colname){
-  new_data <- data %>%
-    dplyr::select(ind_colname)
+pseudoR2 <- function(model, data, respv_colname){
+  
+  data <- data %>%
+    dplyr::rename(Measured = respv_colname)
+  
+  data$Predicted <- predict(model, newdata = data) %>% exp()
+
+ pseudoR2 <- 1 - 
+   sum((data$Measured - data$Predicted)^2) / 
+   sum((data$Measured - mean(data$Measured))^2)  
+ 
+ return(pseudoR2)
 }
+
+`%>%` <- magrittr::`%>%`
+pseudoR2(model = mod, data = dat, resp_colname = "y")
+
+plot(dat$x, dat$y)
