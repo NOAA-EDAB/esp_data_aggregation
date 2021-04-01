2a5cca1be18e08c4f831c56404d190e7c896c94a
Abigail <abigail.tyrell@noaa.gov>
2021-04-01 15:11:05 -0400

    trying to install shiny package

---
+++ R-scripts/install and run app.R
@@ -0,0 +1,4 @@
+remotes::install_github(repo = "NOAA-EDAB/esp_data_aggregation", 
+                        ref = "shiny",
+                        upgrade = "never", 
+                        dependencies = FALSE)
