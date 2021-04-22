42aea5e49b86aa52dec1247777bbe94486691974
Abigail <abigail.tyrell@noaa.gov>
2021-04-22 16:19:04 -0400

    compare swept numbers

--- black-sea-bass/create_small_swept.R
+++ black-sea-bass/create_small_swept.R
@@ -11,24 +11,25 @@ og_pull$survdat$STRATUM <- as.numeric(og_pull$survdat$STRATUM)
 
 mod_data <- survdat::strat_prep(
   surveyData = og_pull$survdat %>%
-    dplyr::filter(SVSPP == 141#, # bsb only
-                 # LENGTH < 19 # small fish only
-                  ) %>% 
+    dplyr::filter(
+      SVSPP == 141 # , # bsb only
+      # LENGTH < 19 # small fish only
+    ) %>%
     dplyr::filter(SEASON == "FALL"),
- #  dplyr::filter(SEASON == "SPRING"),
+  #  dplyr::filter(SEASON == "SPRING"),
   areaPolygon = shape,
   areaDescription = "STRATA"
 )
 
 mean_info <- survdat::strat_mean(mod_data,
-                                 areaDescription = "STRATA",
-                                 seasonFlag = TRUE,
-                                 poststratFlag = FALSE
+  areaDescription = "STRATA",
+  seasonFlag = TRUE,
+  poststratFlag = FALSE
 )
 
 test <- survdat::swept_area(mod_data,
-                            stratmeanData = mean_info,
-                            areaDescription = "STRATA"
+  stratmeanData = mean_info,
+  areaDescription = "STRATA"
 )
 # test
 
@@ -37,7 +38,7 @@ data <- test %>%
   dplyr::mutate(Species = "Black sea bass")
 # data
 
-#write.csv(data, here::here("black-sea-bass", "swept_area_info_spring.csv"))
+# write.csv(data, here::here("black-sea-bass", "swept_area_info_spring.csv"))
 write.csv(data, here::here("black-sea-bass", "swept_area_info_fall.csv"))
 
 spring <- read.csv(here::here("black-sea-bass", "swept_area_info_spring.csv"))
@@ -51,5 +52,51 @@ fall <- fall %>%
 all <- rbind(spring, fall)
 write.csv(all, here::here("black-sea-bass", "swept_area_info_all_bsb.csv"))
 
-NEesp::plot_swept(all, var = "abundance")
-NEesp::plot_swept(all, var = "biomass")
+big <- read.csv(here::here("black-sea-bass", "swept_area_info_all_bsb.csv"))
+
+big_abun <- NEesp::plot_swept(big, var = "abundance")
+big_bio <- NEesp::plot_swept(big, var = "biomass")
+
+small <- read.csv(here::here("black-sea-bass", "swept_area_info_small_bsb.csv"))
+
+small_abun <- NEesp::plot_swept(small, var = "abundance")
+small_bio <- NEesp::plot_swept(small, var = "biomass")
+
+pkg_abun <- NEesp::plot_swept(NEesp::swept %>% dplyr::filter(Species == "Black sea bass"), var = "abundance")
+pkg_bio <- NEesp::plot_swept(NEesp::swept %>% dplyr::filter(Species == "Black sea bass"), var = "biomass")
+
+plots <- c(rbind(
+  ggpubr::ggpar(list(
+    pkg_abun + ggplot2::labs(title = "Package abundance"),
+    big_abun + ggplot2::labs(title = "All BSB sizes abundance"),
+    small_abun + ggplot2::labs(title = "Small BSB sizes abundance")
+  ),
+  ylim = c(-10^7, 2 * 10^8)
+  ),
+  ggpubr::ggpar(list(
+    pkg_bio + ggplot2::labs(title = "Package biomass"),
+    big_bio + ggplot2::labs(title = "All BSB sizes biomass"),
+    small_bio + ggplot2::labs(title = "Small BSB sizes biomass")
+  ),
+  ylim = c(-2*10^6, 2 * 10^7)
+  )
+)
+)
+
+tiff(
+  file = here::here("black-sea-bass", "small_v_all.tiff"),
+  # onefile = TRUE,
+  width = 8,
+  height = 8,
+  units = "in",
+  res = 200,
+  compression = "lzw"
+)
+ggpubr::ggarrange(
+  plotlist = plots,
+  nrow = 3,
+  ncol = 2,
+  common.legend = TRUE,
+  legend = "top"
+)
+dev.off()
