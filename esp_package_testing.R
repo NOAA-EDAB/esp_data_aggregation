1fc267437d7d17fc2fcef721b7b7347c60f979d1
Ricky Tabandera <ricky.tabandera>
2021-04-29 13:24:17 -1000

    commercia data and rmd moved to bookdown

--- esp_package_testing.R
+++
@@ -1,3 +0,0 @@
-remotes::install_github("NOAA-EDAB/esp_data_aggregation@package", dependencies = FALSE)
-NEesp::render_ind_report("Black sea bass", input = "bookdown")
-setwd("~/EDAB/esp_data_aggregation")
