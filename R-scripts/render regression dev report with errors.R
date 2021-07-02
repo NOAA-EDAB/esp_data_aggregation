9108ddc36b8a3ad1980a2d0097563e096238f5d6
Abigail <abigail.tyrell@noaa.gov>
2021-07-02 11:33:43 -0400

    render only one report, skip report deploy

--- R-scripts/render regression dev report with errors.R
+++ R-scripts/render regression dev report with errors.R
@@ -107,17 +107,17 @@ suppressWarnings({
     }
     
     # make 1 lag reports
-    test <- try(NEesp::render_reg_report(
-      stock_var = species[i, 1],
-      epus_var = species[i, 3],
-      region_var = species[i, 2],
-      lag_var = 1,
-      remove_var = FALSE,
-      save_var = TRUE,
-      input = x,
-      parent_folder = "one_year_lag",
-      trouble = FALSE
-    ))
+#    test <- try(NEesp::render_reg_report(
+#      stock_var = species[i, 1],
+#      epus_var = species[i, 3],
+#      region_var = species[i, 2],
+#      lag_var = 1,
+#      remove_var = FALSE,
+#      save_var = TRUE,
+#      input = x,
+#      parent_folder = "one_year_lag",
+#      trouble = FALSE
+#    ))
     
     if (class(test) == "try-error") {
       this_output <- error_analysis()
@@ -126,17 +126,17 @@ suppressWarnings({
     }
     
     # make 1 lag remove reports
-    test <- try(NEesp::render_reg_report(
-      stock_var = species[i, 1],
-      epus_var = species[i, 3],
-      region_var = species[i, 2],
-      lag_var = 1,
-      remove_var = TRUE,
-      save_var = TRUE,
-      input = x,
-      parent_folder = "one_year_lag_remove_recent",
-      trouble = FALSE
-    ))
+#    test <- try(NEesp::render_reg_report(
+#      stock_var = species[i, 1],
+#      epus_var = species[i, 3],
+#      region_var = species[i, 2],
+#      lag_var = 1,
+#      remove_var = TRUE,
+#      save_var = TRUE,
+#      input = x,
+#      parent_folder = "one_year_lag_remove_recent",
+#      trouble = FALSE
+#    ))
     
     if (class(test) == "try-error") {
       this_output <- error_analysis()
@@ -145,6 +145,7 @@ suppressWarnings({
     }
     
     sink()
+    print(test)
     print(this_output)
     print(output)
     print(paste("Done with", i))
