f565cec1f31b18d0d5b8f0063db8e7f89e07672f
Abigail <abigail.tyrell@noaa.gov>
2021-07-02 17:05:00 -0400

    update to run for all species

--- R-scripts/render regression dev report with errors.R
+++ R-scripts/render regression dev report with errors.R
@@ -2,82 +2,7 @@
 `%>%` <- magrittr::`%>%`
 output <- c()
 
-species <- NEesp::regression_species_regions[1,]
-
-# the dumb way to try to force through errors
-error_analysis <- function(fchunk_name = chunk_name,
-                           flast_known = last_known,
-                           ftest = test){
-  problem_file <- NEesp::find_files(
-    paste0("\\{r", "(.{1,5})", fchunk_name),
-    location
-  ) %>% invisible()
-
-  if (problem_file == "Not found") {
-    problem_file2 <- NEesp::find_files(
-      paste0("\\{r", "(.{1,5})", flast_known),
-      location
-    ) %>% invisible()
-
-  } else {
-    problem_file2 <- ""
-  }
-  
-  this_output <- cat(
-    toString(species[i,]), 
-    "0lag", 
-    ftest[1],
-    fchunk_name,
-    problem_file,
-    problem_file2,
-    sep = "\n"
-  )
-  
-  return(this_output)
-}
-
-# the original way
-error_analysis2 <- function(fchunk_name = chunk_name,
-                           flast_known = last_known,
-                           ftest = test){
-  problem_file <- NEesp::find_files(
-    paste0("\\{r", "(.{1,5})", fchunk_name),
-    location
-  ) %>% invisible()
-  
-  problem_file <- problem_file %>%
-    tibble::as_tibble() 
-  
-  if (problem_file == "Not found") {
-    problem_file2 <- NEesp::find_files(
-      paste0("\\{r", "(.{1,5})", flast_known),
-      location
-    ) %>% invisible()
-    
-    problem_file2 <- problem_file2 %>%
-      tibble::as_tibble()
-    
-    file_name <- problem_file2[, 1] %>%
-      stringr::str_split("correlation_bookdown_template/", n = 2)
-    file_name <- file_name[[1]][2]
-    
-    this_output <- c(
-      i, "0lag", ftest[1],
-      paste("unknown - last known file:", file_name),
-      fchunk_name,
-      paste("unknown - last known line:", problem_file2[,2])
-    )
-  } else {
-    
-    file_name <- problem_file[, 1] %>%
-      stringr::str_split("correlation_bookdown_template/", n = 2)
-    file_name <- file_name[[1]][2]
-    
-    this_output <- c(i, toString(ftest[1]), file_name, fchunk_name, problem_file[, 2],
-                     recursive = TRUE)
-  }
-  return(this_output)
-}
+species <- NEesp::regression_species_regions
 
 suppressWarnings({
   for (i in 1:nrow(species)) {
@@ -100,76 +25,58 @@ suppressWarnings({
       trouble = FALSE
     ))
 
- #   if (class(test) == "try-error") {
+   if (class(test) == "try-error") {
     print("ERROR ANALYSIS")
+    print(test)
     print(chunk_name)
     print(last_known)
-     # this_output <- error_analysis()
-    #  print(this_output)
-      #output <- rbind(output, this_output)
-     # output <- cat(output, this_output)
-      #print(output)
-  #  }
+    }
     
     # make 1 lag reports
-#    test <- try(NEesp::render_reg_report(
-#      stock_var = species[i, 1],
-#      epus_var = species[i, 3],
-#      region_var = species[i, 2],
-#      lag_var = 1,
-#      remove_var = FALSE,
-#      save_var = TRUE,
-#      input = x,
-#      parent_folder = "one_year_lag",
-#      trouble = FALSE
-#    ))
+    test <- try(NEesp::render_reg_report(
+      stock_var = species[i, 1],
+      epus_var = species[i, 3],
+      region_var = species[i, 2],
+      lag_var = 1,
+      remove_var = FALSE,
+      save_var = TRUE,
+      input = x,
+      parent_folder = "one_year_lag",
+      trouble = FALSE
+    ))
     
-#    if (class(test) == "try-error") {
-#      this_output <- error_analysis()
-#      #output <- rbind(output, this_output)
-#      output <- cat(output, this_output)
-#    }
+    if (class(test) == "try-error") {
+      print("ERROR ANALYSIS")
+      print(test)
+      print(chunk_name)
+      print(last_known)
+    }
     
     # make 1 lag remove reports
-#    test <- try(NEesp::render_reg_report(
-#      stock_var = species[i, 1],
-#      epus_var = species[i, 3],
-#      region_var = species[i, 2],
-#      lag_var = 1,
-#      remove_var = TRUE,
-#      save_var = TRUE,
-#      input = x,
-#      parent_folder = "one_year_lag_remove_recent",
-#      trouble = FALSE
-#    ))
+    test <- try(NEesp::render_reg_report(
+      stock_var = species[i, 1],
+      epus_var = species[i, 3],
+      region_var = species[i, 2],
+      lag_var = 1,
+      remove_var = TRUE,
+      save_var = TRUE,
+      input = x,
+      parent_folder = "one_year_lag_remove_recent",
+      trouble = FALSE
+    ))
     
-#    if (class(test) == "try-error") {
-#      this_output <- error_analysis()
-#      #output <- rbind(output, this_output)
-#      output <- cat(output, this_output)
-#    }
+    if (class(test) == "try-error") {
+      print("ERROR ANALYSIS")
+      print(test)
+      print(chunk_name)
+      print(last_known)
+    }
     
 #    sink()
-    print(test)
-#    print(this_output)
-#    print(output)
-    print(paste("Done with", i))
+
+    print(paste("Done with", NEesp::regression_species_regions[i,]))
   }
 
-  if (class(output) == "NULL") {
-
-    dir.create(here::here("logs"))
-  
-  } else {
-    
-    colnames(output) <- c("Species", "Report type", "Error", "File throwing error", "Chunk name", "Line throwing error")
-
-    file <- paste0("logs/", Sys.time(), ".csv") %>%
-      stringr::str_replace_all(":", ".")
-    dir.create(here::here("logs"))
-    
-    write.csv(output, here::here(file), row.names = FALSE)
-    
     # clean up .Rmd and .yml files that didn't render
     files_to_remove <- c(list.files(here::here("Regressions"), 
                                     pattern = "\\.Rmd$",
@@ -180,5 +87,4 @@ suppressWarnings({
                                     recursive = TRUE,
                                     full.names = TRUE))
     file.remove(files_to_remove)
-  }
-})
+  })
